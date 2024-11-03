d9f9332925c0b99ca617bfafac12275a9dc30fa2
Jeffrey Seneff <jeffsnff@gmail.com>
2024-11-03 09:34:27 -0500

    Pushing up CSharp mooc exercises

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1{
+  internal class Program{
+    static void Main(string[] args){
+
+      PrintChristmasTree(10);
+      
+    }
+
+    public static void PrintStars(int numStars){
+      for(int i = 1; i<=numStars; i++){
+        Console.Write("*");
+        if(i==numStars){
+          Console.WriteLine("");
+        }
+      }
+    }
+
+    public static void PrintSquare(int squareSize){
+      for(int i = 1; i<=squareSize; i++){
+        PrintStars(squareSize);
+      }
+    }
+
+    public static void PrintRectangle(int width, int height){
+      for(int countHeight = 1; countHeight <= height; countHeight++){
+        PrintStars(width);
+      }
+    }
+
+    public static void PrintTriangle(int height){
+      for(int countHeight = 1; countHeight<=height; countHeight++){
+        PrintStars(countHeight);
+      }
+    }
+
+    public static void PrintSpaces(int spaces){
+      spaces--;
+      for(int countSpaces = 1; countSpaces<=spaces; countSpaces++){
+        Console.Write(" ");
+      }
+    }
+
+    public static void PrintRightTriangle(int size){
+      int numStars = 1;
+      for(int count = size; count>=1; count--){
+        PrintSpaces(count);
+        PrintStars(numStars);
+        numStars++;
+      }
+    }
+
+    public static void PrintChristmasTree(int height){
+
+      int numStars = 1;
+      for(int count = height; count>=1; count--){
+        PrintSpaces(count);
+        PrintStars(numStars);
+        numStars = numStars + 2;
+      }
+      for(int i = 1; i<=2; i++){
+        PrintSpaces(height-1);
+        PrintStars(3);
+      }
+    }
+  }
+}
---
+++ ListsAndArrays/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ListsAndArrays{
+  internal class Program{
+    static void Main(string[] args){
+      Console.WriteLine("Hello from List and Arrays");
+    }
+  }
+}
