78c8799ad4d285e1551c6cbc9a5d66cffcf3816e
Professor Chaos <22498613+jeffsnff@users.noreply.github.com>
2024-11-06 10:10:28 -0500

    Pushing from github.dev

--- ListsAndArrays/Program.cs
+++ ListsAndArrays/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 
 namespace ListsAndArrays{
-  internal class Program{
-    static void Main(string[] args){
-      Console.WriteLine("Hello there!");
+  internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello CSharp Mooc Repo from Github code spaces!");
+        }
     }
-  }
 }
